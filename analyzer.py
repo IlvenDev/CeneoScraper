2ba190f84db83c728546292c09e64fe6a7432409
Patryk <225972@student.uek.krakow.pl>
2023-04-23 13:04:59 +0200

    2023-04-23 PF - Fix module dependencies, Fix pros/cons_count in analyer

--- analyzer.py
+++ analyzer.py
@@ -11,8 +11,8 @@ opinions = pd.read_json(f"./opinions/{product_code}.json")
 opinions_count = len(opinions.index)
 # pros_count = sum([False if len(p)== 0 else True for p in opinions.pros])
 # cons_count = sum([False if len(c)== 0 else True for c in opinions.cons])
-pros_count = opinions.pros.map(lambda p: False if len(p)== 0 else True for p in opinions.pros).sum()
-cons_count = opinions.pros.map(lambda c: False if len(c)== 0 else True for c in opinions.pros).sum()
+pros_count = opinions.pros.map(lambda p: False if len(p)==0 else True).sum()
+cons_count = opinions.cons.map(lambda c: False if len(c)==0 else True).sum()
 avg_score = 0
 
-print(opinions.pros)
+print(pros_count)
